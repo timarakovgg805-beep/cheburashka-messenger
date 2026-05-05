ea0012aea7f44b7b91ed5385e6d78a7bc9bf407a
Cheburashka <cheburashka@messenger.local>
2026-05-05 20:47:23 +0300

    Fix duplicate route

--- pon-server.js
+++ pon-server.js
@@ -446,10 +446,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pon.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pon.html'));
-});
-
 app.get('/favicon.ico', (req, res) => {
     res.status(204).end();
 });
